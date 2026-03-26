0e9bf12cbba19b5da87fc8aff0722692b42e9442
Arthur Macedo F. F. <146777649+arthurmacedoo@users.noreply.github.com>
2026-03-26 10:06:32 -0400

    Update index.tsx

--- frontend/src/screens/authentication/resetPassword/index.tsx
+++ frontend/src/screens/authentication/resetPassword/index.tsx
@@ -1,1 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Input } from '../../../components/input'
+import { styles } from './styles'
+
+export function ResetPasswordScreen() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../../../../assets/logo.jpeg')}
+                style={styles.image}
+            />
+
+            <View>
+                <Input
+                    labelProp='Nova Senha'
+                    placeholderProp='Digite a nova senha'
+                    keyboardTypeProp='default'
+                    isPassword={true} />
+
+                <Input
+                    labelProp='Confirmar Senha'
+                    placeholderProp='Digite a mesma senha'
+                    keyboardTypeProp='default'
+                    isPassword={true} />
+
+                <Text style={styles.voltarLogin}>Lembrou a senha? <Text style={styles.LoginLink}>Login</Text></Text>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonLabel}>Salvar Senha</Text>
+                </TouchableOpacity>
+            </View>
+
+
+            <StatusBar style="auto" />
+        </View>
+    );
+}
 
